d4228a5b13d1342b0b9665f603ccfab9adb88328
shaharrubinshtein <tody0489@gmail.com>
2018-12-23 22:28:41 +0200

    update1
    update1

---
+++ index.js
@@ -0,0 +1,44 @@
+var events      = require('events'),
+    restaurant  = require('./resturantClass'),
+    express     = require('express'),
+    config      = require('./config').events;
+    port        =process.env.PORT || 5000;
+
+var restaurantOrders = new restaurant(),
+       app          = express()
+       
+       app.get('/',(req,res)=>{
+      
+        //demo
+        restaurantOrders.addOrder(2);
+        restaurantOrders.addOrder(3);
+        restaurantOrders.addOrder(7);
+        restaurantOrders.cancelOrder(1);
+        restaurantOrders.resetOrders();
+        restaurantOrders.addOrder(1);
+       res.send(JSON.stringify(restaurantOrders.massageLog));/////////////////////////////////////////////////////////////////////////////
+       res.end();
+       
+
+     });
+   
+     app.listen(port,()=>{console.log(`listening on port ${port}!`);
+    })
+       restaurantOrders
+                  .on(config.ALL_ORDERS,restaurantOrders.getAllOrders)
+                  .on(config.EMPTY,restaurantOrders.resetOrders)
+                  .on(config.FULL,restaurantOrders.checkOrder)
+                  .on(config.ADD_OR_CANCEL,restaurantOrders.orderChange)
+                  .on(config.CANCEL_ERROR,restaurantOrders.errorCancelOrder);
+    
+        return restaurantOrders;
+   
+
+
+
+
+
+
+
+
+
